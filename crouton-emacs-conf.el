0a92769214b765971dd620eb43a9ad4e97a7d5ae
Aurelien Lourot <aurelien.lourot@gmail.com>
2015-07-04 19:08:48 +0200

    Support for Del key.

--- crouton-emacs-conf.el
+++ crouton-emacs-conf.el
@@ -5,6 +5,12 @@
 ;; Middle-click support. See http://lourot.com/articles/crouton-emacs-middle-click
 (global-set-key (kbd "<M-mouse-2>") 'mouse-yank-primary)
 
+;; Delete support.
+;; You can make [Shift] + [Backspace] behave like [Del] with
+;; xmodmap -e "keycode 22 shift = BackSpace Delete"
+;; See http://lourot.com/articles/crouton-caps-lock
+(global-set-key (kbd "<S-delete>") 'delete-char)
+
 ;; Makes emacs kill (i.e. copy) to the clipboard.
 ;; See http://stackoverflow.com/questions/64360/how-to-copy-text-from-emacs-to-another-application-on-linux
 (setq x-select-enable-clipboard t)
